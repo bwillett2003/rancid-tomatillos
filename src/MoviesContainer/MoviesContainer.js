e7a255147ac6b4bb82460eee10c9c0726f886240
Bryan Willett <btwillett2003@gmail.com>
2024-12-12 16:50:59 -0600

    Typo with vote_average, should be vote_count.

--- src/MoviesContainer/MoviesContainer.js
+++ src/MoviesContainer/MoviesContainer.js
@@ -5,7 +5,7 @@ function Movies({ movies }) {
   return (
       <section className='MoviesContainer'>
         {movies.map(movie => (
-        <MoviePoster key={movie.id} title={movie.title} poster={movie.poster_path} votes={movie.vote_average} />
+        <MoviePoster key={movie.id} title={movie.title} poster={movie.poster_path} votes={movie.vote_count} />
       ))}
       </section>
   );
