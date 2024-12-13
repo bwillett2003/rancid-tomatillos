312e1803ce6ad6bf87ef36ab4231384073f37666
James-Cochran <jameslikesfruit@gmail.com>
2024-12-12 20:48:20 -0500

    continue CSS styling for movie posters, close but still not quite there.

--- src/MoviePoster/MoviePoster.js
+++ src/MoviePoster/MoviePoster.js
@@ -4,7 +4,7 @@ function MoviePoster({ title, poster, votes }) {
   return (
     <section className='MoviePoster'>
       <img src={poster} alt={`${title} poster`} />
-      <p>{votes}</p>
+      <p className="vote_count">{votes}</p>
     </section>
   );
 }
