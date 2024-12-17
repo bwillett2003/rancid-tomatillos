a0efe58fca6977d79004ca97cff119c638d51d04
James-Cochran <jameslikesfruit@gmail.com>
2024-12-17 16:07:56 -0500

    added more cypress tests for movie_container / homepage - can't get upvote to work.

--- cypress/e2e/movie_container.cy.js
+++ cypress/e2e/movie_container.cy.js
@@ -43,4 +43,34 @@ describe('movie container spec', () => {
     cy.get('.VoteContainer').last().find('img[alt="Upvote"]').should('exist')
     cy.get('.VoteContainer').last().find('img[alt="Downvote"]').should('exist')
   })
+
+  it('sends you to the movie details page when a movie is selected', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos-api.onrender.com/api/v1/movies/155', {
+      statusCode: 200,
+      fixture: 'movie_details'
+    })
+    cy.get('.MoviePoster').first().click()
+    cy.visit('http://localhost:3000/155')
+    // should go to a new page (specifically movie details for movie id /155)
+    cy.url().should('include', '/155')
+  })
+
+  it('adds one vote to the count when the upvote img is clicked', () => {
+    cy.intercept('PATCH', "https://rancid-tomatillos-api.onrender.com/api/v1/movies/155", {
+      statusCode: 201,
+      body: {
+        id: 155,
+        poster_path: "https://image.tmdb.org/t/p/original//qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+        title: "The Dark Knight",
+        vote_count: 32544
+      }
+    })
+    
+    cy.get('.VoteContainer').first('p').find('p').should('contain', '32544')
+    cy.get('.vote-button').first().click()
+    // cy.get('.VoteContainer').first().find('img[alt="Upvote"]').should('exist').click()
+    // cy.get('.VoteContainer').first('p').find('p').should('contain', '32545')
+    // cy.get('.vote-button').first().click()
+  })
+
 })
